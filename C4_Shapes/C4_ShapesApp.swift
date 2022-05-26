6ad45952110e81a7d4a1fd65fdeb1a32104a76b9
Jae Cho <jjaecho@gmail.com>
2022-05-26 17:23:49 -0400

    Initial Commit

---
+++ C4_Shapes/C4_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4_ShapesApp.swift
+//  C4_Shapes
+//
+//  Created by Jae Cho on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct C4_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
