7c897b4fe7d854dbfcc71be0c9f082c12000b831
Jae Cho <jjaecho@gmail.com>
2022-05-27 13:29:32 -0400

    C4 Making an arrow using path and making an animation, also using ColorCyclingCirline which we learned in Day 44

--- C4_Shapes/ContentView.swift
+++ C4_Shapes/ContentView.swift
@@ -7,10 +7,71 @@
 
 import SwiftUI
 
+//Making a pointed up arrow
+struct Arrow : Shape {
+	
+	func path(in rect: CGRect) -> Path {
+		var path = Path()
+		path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+		path.addLine(to: CGPoint(x: rect.midX - 100, y: rect.minY + 100))
+		path.addLine(to: CGPoint(x: rect.midX - 50, y: rect.minY + 100))
+		path.addLine(to: CGPoint(x: rect.midX - 50, y: rect.minY + 200))
+		path.addLine(to: CGPoint(x: rect.midX + 50, y: rect.minY + 200))
+		path.addLine(to: CGPoint(x: rect.midX + 50, y: rect.minY + 100))
+		path.addLine(to: CGPoint(x: rect.midX + 100, y: rect.minY + 100))
+		path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+		
+		return path
+	}
+}
+
+struct ColorCyclingRectangle : View{
+	var amount = 0.9
+	var steps : Int = 100
+	var body: some View {
+		ZStack{
+			ForEach(0..<steps) { value in
+				Circle()
+					.inset(by: CGFloat(value))
+					.strokeBorder(LinearGradient(gradient:
+						Gradient(colors: [
+							self.color(for: value, brightness: 1),
+							self.color(for: value, brightness: 0.1)
+						])
+						, startPoint: .top,
+						  endPoint: .bottom),
+						lineWidth: 2)
+			}
+		}
+	}
+	
+	func color(for value: Int, brightness: Double) -> Color {
+		var targetHue = Double(value) / Double(self.steps) + self.amount
+		
+		if targetHue > 1 { targetHue -= 1 }
+		return Color(hue: targetHue, saturation: 1, brightness: brightness)
+	}
+}
+
+
 struct ContentView: View {
+	@State private var thiknessAmount = 10.0
+	@State private var colorCycle = 0.0
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		 
+		 VStack {
+			 Arrow()
+				 .stroke(.blue, style: StrokeStyle(lineWidth: thiknessAmount, lineCap: .round, lineJoin: .round))
+				 .onTapGesture {
+					 withAnimation {
+						 thiknessAmount = Double.random(in: 10...90)
+					 }
+				 }
+			 ColorCyclingRectangle(amount: self.colorCycle)
+				 .frame(width: 300, height: 300)
+			 Slider(value: $colorCycle)
+		 }
     }
 }
 
